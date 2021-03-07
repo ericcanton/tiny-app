bdeb7aae7bcaec08966a5fbe0bc4f3201abed46b
ericcanton <57836255+ericcanton@users.noreply.github.com>
2021-03-07 09:39:05 -0600

    Basic request working.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use colored::*;
+
+use std::io::Read;
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("https://wj2o3qsbc8.execute-api.us-east-1.amazonaws.com/prod/yo?name=eric")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    println!("Body:\n{}", body.green());
+
+    Ok(())
 }
