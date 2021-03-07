dff8396dbf0d835923e06212901a14612f584eba
ericcanton <57836255+ericcanton@users.noreply.github.com>
2021-03-07 16:59:32 -0600

    Request payloads parsing well into struct.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,48 @@
 use lambda_http::{lambda, IntoResponse, Request, RequestExt};
 use lambda_runtime::{Context, error::HandlerError};
+use serde_json;
+use serde_derive::{Serialize, Deserialize};
 
 
-fn main() {
-    lambda!(hello)
+#[derive(Deserialize, Serialize, Default)]
+struct UserState {
+    #[serde(default)]
+    a: f32,
+    #[serde(default)]
+    b: f32,
 }
 
-fn hello(
+#[derive(Deserialize, Serialize)]
+enum InternalCode {
+    Ok,
+    AwsError,
+}
+
+#[derive(Deserialize, Serialize)]
+struct ApiResponse {
+    internal_code: InternalCode,
+    user_state: UserState
+}
+
+fn main() {
+    lambda!(update_state)
+}
+
+
+fn update_state(
     request: Request,
     _ctx: Context
 ) -> Result<impl IntoResponse, HandlerError> {
-    Ok(format!(
-        "hello {}",
-        request
-            .query_string_parameters()
-            .get("name")
-            .unwrap_or_else(|| "stranger")
-    ))
+    let mut ud: UserState = request.payload()
+        .unwrap_or_else(|_parse_err| None)
+        .unwrap_or_default();
+
+    println!("{{a: {}, b: {}}}", ud.a, ud.b);
+
+    ud.a = ud.a + 1.0;
+    ud.b = 2.0 * ud.b;
+
+    println!("{{a: {}, b: {}}}", ud.a, ud.b);
+
+    Ok(serde_json::to_string(&ud).unwrap())
 }
